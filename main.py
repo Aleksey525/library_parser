5c32630c72a0141411803d18cef3894b2a7cec30
Aleksey525 <alex_tolchin@mail.ru>
2024-06-03 13:43:31 +0300

    omtimized the code

--- main.py
+++ main.py
@@ -1,24 +1,34 @@
 import os
 import requests
-import urllib.parse
 
 
-def download_books(path):
+
+def download_books(path, book_id):
     os.makedirs(path, exist_ok=True)
-    id = 1
-    while id <= 10:
-        url_template = 'https://tululu.org/txt.php?id={}/'.format(id)
-        response = requests.get(url_template)
-        response.raise_for_status()
-        patch_template = f'{path}/id{id}.txt'
-        with open(patch_template, 'wb') as file:
-            file.write(response.content)
-        id += 1
+    url_template = 'https://tululu.org/txt.php?id={}/'.format(book_id)
+    response = requests.get(url_template)
+    check_for_redirect(response)
+    response.raise_for_status()
+    patch_template = f'{path}/id{book_id}.txt'
+    with open(patch_template, 'wb') as file:
+        file.write(response.content)
+
+
+def check_for_redirect(response):
+    if response.history:
+        raise requests.exceptions.HTTPError
 
 
 def main():
     path = 'books'
-    download_books(path)
+    books_id = 1
+    os.makedirs(path, exist_ok=True)
+    while books_id <= 10:
+        try:
+            download_books(path, books_id)
+        except requests.exceptions.HTTPError:
+            print('перенаправление')
+        books_id += 1
 
 
 if __name__ == '__main__':
